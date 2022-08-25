554df1c4011cefc171ca2e81df36b2bbcc621715
avonWork <git config --global user.email avonWork@gmail.com>
2022-08-25 10:58:03 +0800

    index-finish

--- source/js/all.js
+++ source/js/all.js
@@ -12,13 +12,34 @@ $(function () {
   //   // console.log(e.delegateTarget); //document
   // });
 
-  // --我的最愛--
-  $('.tab-sweet-hart').on('click', function (e) {
-    //console.log(e); //('.tab-sweet-hart') 
-    //console.log(e.delegateTarget); //('.tab-sweet-hart')
-    let target = e.delegateTarget;
-    $(target).toggleClass('tab-sweet-hart-fav');
+  // --我的最愛-- 桌機 手機失效
+  // $('.tab-sweet-hart').on('click', function (e) {
+  //   //console.log(e); //('.tab-sweet-hart') 
+  //   //console.log(e.delegateTarget); //('.tab-sweet-hart')
+  //   let target = e.delegateTarget;
+  //   console.log(target);
+  //   $(target).toggleClass('tab-sweet-hart-fav');
+  // });
+
+  // --我的最愛--icon 支援手機桌機
+  $('.tab-sweet-hart').on({
+    touchend: function (e) {
+      let target = e.delegateTarget;
+      e.preventDefault()
+      $(target).toggleClass('tab-sweet-hart-fav')
+    },
+    click: function (e) {
+      let target = e.delegateTarget;
+      e.preventDefault()
+      $(target).toggleClass('tab-sweet-hart-fav')
+    }
   });
+
+  // $('.tab-sweet-hart').click(function () {
+  //   console.log($(this).eq(0));
+  //   $(this).toggleClass('tab-sweet-hart-fav');
+  // });
+
 
   // $('.tab-sweet-hart').on('click', function () {
   //   console.log($(this)); //object
